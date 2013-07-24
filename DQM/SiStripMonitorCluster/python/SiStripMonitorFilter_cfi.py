c92fda9986a99a666d83bf827b22b6a390d314a4
Mia <mia.tosi@cern.ch>
2013-07-24 12:33:49 +0200

    first getByLabel ---> getByToken transition

--- DQM/SiStripMonitorCluster/python/SiStripMonitorFilter_cfi.py
+++ DQM/SiStripMonitorCluster/python/SiStripMonitorFilter_cfi.py
@@ -4,7 +4,7 @@ SiStripMonitorFilter = cms.EDAnalyzer("SiStripMonitorFilter",
     # by default do not write out any file with histograms
     # can overwrite this in .cfg file with: replace SiStripMonitorFilter.OutputMEsInRootFile = true
     OutputMEsInRootFile = cms.bool(False),
-    FilterProducer = cms.string('ClusterMTCCFilter'),
+    FilterProducer = cms.InputTag('ClusterMTCCFilter'),
     OutputFileName = cms.string('dqm_sistrip_filter.root')
 )
 
--- DQM/SiStripMonitorCluster/python/SiStripMonitorHLT_cfi.py
+++ DQM/SiStripMonitorCluster/python/SiStripMonitorHLT_cfi.py
@@ -4,7 +4,7 @@ SiStripMonitorHLT = cms.EDAnalyzer("SiStripMonitorHLT",
     # by default do not write out any file with histograms
     # can overwrite this in .cfg file with: replace SiStripMonitorHLT.OutputMEsInRootFile = true
     OutputMEsInRootFile = cms.bool(False),
-    HLTProducer = cms.string('trigger'),
+    HLTProducer = cms.InputTag('ClusterMTCCFilter'),
     OutputFileName = cms.string('test_digi_hlt.root')
 )
 
