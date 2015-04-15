d6512f12b82c6a9e4570dfc867a567a991a6a35c
Mia <mia.tosi@cern.ch>
2015-04-15 09:45:16 +0200

    add tracking online DQM

--- DQM/HLTEvF/python/HLTWorkspace_cff.py
+++ DQM/HLTEvF/python/HLTWorkspace_cff.py
@@ -6,7 +6,19 @@ import FWCore.ParameterSet.Config as cms
 #from DQM.HLTEvF.OccupancyPlotter_cfi import *
 
 from DQM.HLTEvF.HLTWorkspace_cfi import *
-hlt4vector = cms.Path(hltWorkspace)
+# strip cluster monitor
+from DQMOffline.Trigger.SiStrip_OfflineMonitoring_cff import *
+# tracking monitor
+from DQMOffline.Trigger.TrackingMonitoring_cff import *
+iterHLTTracksMonitoringHLT.doProfilesVsLS   = cms.bool(True)
+
+hlt4vector = cms.Path(
+    hltWorkspace
+#    * sistripMonitorHLTsequence # strip cluster monitoring
+    * pixelTracksMonitoringHLT # hltPixel tracks monitoring
+    * iterHLTTracksMonitoringHLT # hltIter2Merged tracks monitoring
+)
 
 #hlt4vector = cms.Path(onlineOccPlot * hltWorkspace)
 #hlt4vector = cms.Path(onlineOccPlot)
+
