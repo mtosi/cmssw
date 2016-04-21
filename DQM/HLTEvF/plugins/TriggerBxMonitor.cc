c0fd0a0e5dfa65f68313d04b1b1dd4e09360a45d
Mia <mia.tosi@cern.ch>
2016-04-21 20:24:58 +0200

    add possibility of selecting list of HLT paths

--- DQM/HLTEvF/plugins/TriggerBxMonitor.cc
+++ DQM/HLTEvF/plugins/TriggerBxMonitor.cc
@@ -86,6 +86,12 @@ private:
 
   // HLT triggers
   std::vector<TH1F *>     m_hlt_bx;
+
+  std::vector<std::string> const   m_hlt2monitor;
+  bool m_doPlots4l1;
+  bool m_doPlots4hlt;
+  std::vector<std::string>    m_hltName2monitor;
+  std::vector<int>            m_hltID2monitor;
 };
 
 
@@ -93,9 +99,12 @@ private:
 void TriggerBxMonitor::fillDescriptions(edm::ConfigurationDescriptions & descriptions)
 {
   edm::ParameterSetDescription desc;
+  desc.addUntracked<bool>("doPlots4l1",  true);
+  desc.addUntracked<bool>("doPlots4hlt", true);
   desc.addUntracked<edm::InputTag>( "l1tResults",       edm::InputTag("gtDigis"));
   desc.addUntracked<edm::InputTag>( "hltResults",       edm::InputTag("TriggerResults"));
   desc.addUntracked<std::string>(   "dqmPath",          "HLT/TriggerBx" );
+  desc.addUntracked<std::vector<std::string> >( "hltTriggerPaths", {} );
   descriptions.add("triggerBxMonitor", desc);
 }
 
@@ -114,7 +123,12 @@ TriggerBxMonitor::TriggerBxMonitor(edm::ParameterSet const & config) :
   m_l1t_algo_bx(),
   m_l1t_tech_bx(),
   // HLT triggers
-  m_hlt_bx()
+  m_hlt_bx(),
+  m_hlt2monitor   ( config.getUntrackedParameter<std::vector<std::string> > ("hltTriggerPaths"), {} ),
+  m_doPlots4l1    ( config.getUntrackedParameter<bool> ("doPlots4l1",  true) ),
+  m_doPlots4hlt   ( config.getUntrackedParameter<bool> ("doPlots4hlt", true) ),
+  m_hltName2monitor ( {} ),
+  m_hltID2monitor   ( {} )
 {
 }
 
@@ -125,29 +139,53 @@ TriggerBxMonitor::~TriggerBxMonitor()
 void TriggerBxMonitor::dqmBeginRun(edm::Run const & run, edm::EventSetup const & setup)
 {
   // cache the L1 trigger menu
-  m_l1tMenu     = get<L1GtTriggerMenuRcd, L1GtTriggerMenu>(setup);
-  m_l1tAlgoMask = get<L1GtTriggerMaskAlgoTrigRcd, L1GtTriggerMask>(setup);
-  m_l1tTechMask = get<L1GtTriggerMaskTechTrigRcd, L1GtTriggerMask>(setup);
-  if (m_l1tMenu and m_l1tAlgoMask and m_l1tTechMask) {
-    m_l1t_algo_bx.clear();
-    m_l1t_algo_bx.resize( m_l1tAlgoMask->gtTriggerMask().size(), nullptr );
-    m_l1t_tech_bx.clear();
-    m_l1t_tech_bx.resize( m_l1tTechMask->gtTriggerMask().size(), nullptr );
-  } else {
-    // L1GtUtils not initialised, skip the the L1T monitoring
-    edm::LogError("TriggerBxMonitor") << "failed to read the L1 menu or masks from the EventSetup, the L1 trigger bx distribution  will not be monitored";
+  if (m_doPlots4l1) {
+    m_l1tMenu     = get<L1GtTriggerMenuRcd, L1GtTriggerMenu>(setup);
+    m_l1tAlgoMask = get<L1GtTriggerMaskAlgoTrigRcd, L1GtTriggerMask>(setup);
+    m_l1tTechMask = get<L1GtTriggerMaskTechTrigRcd, L1GtTriggerMask>(setup);
+    if (m_l1tMenu and m_l1tAlgoMask and m_l1tTechMask) {
+      m_l1t_algo_bx.clear();
+      m_l1t_algo_bx.resize( m_l1tAlgoMask->gtTriggerMask().size(), nullptr );
+      m_l1t_tech_bx.clear();
+      m_l1t_tech_bx.resize( m_l1tTechMask->gtTriggerMask().size(), nullptr );
+    } else {
+      // L1GtUtils not initialised, skip the the L1T monitoring
+      edm::LogError("TriggerBxMonitor") << "failed to read the L1 menu or masks from the EventSetup, the L1 trigger bx distribution  will not be monitored";
+    }
   }
 
-  // initialise the HLTConfigProvider
-  bool changed = true;
-  edm::EDConsumerBase::Labels labels;
-  labelsForToken(m_hlt_results, labels);
-  if (m_hltConfig.init(run, setup, labels.process, changed)) {
-    m_hlt_bx.clear();
-    m_hlt_bx.resize( m_hltConfig.size(), nullptr );
-  } else {
-    // HLTConfigProvider not initialised, skip the the HLT monitoring
-    edm::LogError("TriggerBxMonitor") << "failed to initialise HLTConfigProvider, the HLT bx distribution will not be monitored";
+  if (m_doPlots4hlt) {
+    // initialise the HLTConfigProvider
+    bool changed = true;
+    edm::EDConsumerBase::Labels labels;
+    labelsForToken(m_hlt_results, labels);
+
+    if (m_hltConfig.init(run, setup, labels.process, changed)) {
+
+      m_hlt_bx.clear();
+      m_hltName2monitor.clear();
+      m_hltID2monitor.clear();
+
+      if (!m_hlt2monitor.size())
+        m_hltName2monitor = m_hltConfig.triggerNames();
+      else
+	for (auto name : m_hlt2monitor) {
+	  if (m_hltConfig.triggerIndex(name) != m_hltConfig.size())
+	    m_hltName2monitor.push_back(name);
+	}
+
+      for (auto name : m_hltName2monitor) {
+	if (m_hltConfig.triggerIndex(name) != m_hltConfig.size())
+	  m_hltID2monitor.push_back(m_hltConfig.triggerIndex(name));
+	else {
+	}
+      } 
+      //    m_hlt_bx.resize( m_hltConfig.size(), nullptr );
+      m_hlt_bx.resize( m_hltName2monitor.size(), nullptr );
+    } else {
+      // HLTConfigProvider not initialised, skip the the HLT monitoring
+      edm::LogError("TriggerBxMonitor") << "failed to initialise HLTConfigProvider, the HLT bx distribution will not be monitored";
+    }
   }
 }
 
@@ -194,8 +232,10 @@ void TriggerBxMonitor::bookHistograms(DQMStore::IBooker & booker, edm::Run const
   if (m_hltConfig.inited()) {
     // book the HLT triggers rate histograms
     booker.setCurrentFolder( m_dqm_path + "/HLT" );
-    for (unsigned int i = 0; i < m_hltConfig.size(); ++i) {
-      std::string const & name = m_hltConfig.triggerName(i);
+    //    for (unsigned int i = 0; i < m_hltConfig.size(); ++i) {
+    for (unsigned int i = 0; i < m_hlt_bx.size(); ++i) {
+      //      std::string const & name = m_hltConfig.triggerName(i);
+      std::string const & name = m_hltName2monitor[i];
       m_hlt_bx[i] = booker.book1D(name, name, s_bx_range + 1, -0.5, s_bx_range + 0.5)->getTH1F();
     }
   }
@@ -231,13 +271,15 @@ void TriggerBxMonitor::analyze(edm::Event const & event, edm::EventSetup const &
   // monitor the bx distribution for the HLT triggers
   if (m_hltConfig.inited()) {
     edm::TriggerResults const & hltResults = * get<edm::TriggerResults>(event, m_hlt_results);
-    if (hltResults.size() == m_hlt_bx.size()) {
+    //    if (hltResults.size() == m_hlt_bx.size()) {
+    if (hltResults.size() != m_hlt_bx.size() && m_hlt2monitor.size() == 0) {
+      edm::LogWarning("TriggerBxMonitor") << "This should never happen: the number of HLT paths has changed since the beginning of the run";
+    } else {
       for (unsigned int i = 0; i < m_hlt_bx.size(); ++i) {
-        if (hltResults.at(i).accept())
+	//        if (hltResults.at(i).accept())
+        if (hltResults.at(m_hltID2monitor[i]).accept())
           m_hlt_bx[i]->Fill(bx);
       }
-    } else {
-      edm::LogWarning("TriggerBxMonitor") << "This should never happen: the number of HLT paths has changed since the beginning of the run";
     }
   }
 }
