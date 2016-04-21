e0a0ac427d9ac38a9f39f4393fd77f48a288184e
Mia <mia.tosi@cern.ch>
2016-04-21 20:46:11 +0200

    fix l1tResults InputTag (it is needed for getting bx)

--- DQMOffline/Trigger/python/HLTpathsMonitoringVsBX_cff.py
+++ DQMOffline/Trigger/python/HLTpathsMonitoringVsBX_cff.py
@@ -6,7 +6,7 @@ from DQM.HLTEvF.triggerRatesMonitor_cfi import *
 allTriggerPathMonitoringVsBX = triggerBxMonitor.clone(
   doPlots4l1 = cms.untracked.bool(False),
   doPlots4hlt = cms.untracked.bool(True),
-  l1tResults = cms.untracked.InputTag(""),
+  l1tResults = cms.untracked.InputTag('hltGtDigis', '', 'HLT'),
   hltResults = cms.untracked.InputTag('TriggerResults', '', 'HLT'),
   dqmPath = cms.untracked.string('HLT/TriggerBx'),
   hltTriggerPaths = cms.untracked.vstring()
@@ -15,7 +15,7 @@ allTriggerPathMonitoringVsBX = triggerBxMonitor.clone(
 zeroBiasMonitoringVsBX = triggerBxMonitor.clone(
   doPlots4l1      = cms.untracked.bool(False),
   doPlots4hlt     = cms.untracked.bool(True),
-  l1tResults      = cms.untracked.InputTag(""),
+  l1tResults      = cms.untracked.InputTag('hltGtDigis', '', 'HLT'),
   hltResults      = cms.untracked.InputTag('TriggerResults', '', 'HLT'),
   dqmPath         = cms.untracked.string('HLT/TriggerBx'),
   hltTriggerPaths = cms.untracked.vstring( 'HLT_ZeroBias_v', 'HLT_ZeroBias_IsolatedBunches_v', 'HLT_ZeroBias_FirstCollisionAfterAbortGap' )
